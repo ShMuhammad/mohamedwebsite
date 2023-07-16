17bf1c010232016e7b7a4624968a3271bb3d1641
ShMuhammad <14.chaouni@gmail.com>
2023-07-16 23:35:12 +0100

    Final version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let codewarsDiv = document.getElementById("codeware_score");
 codewarsDiv.style.visibility = "hidden";
 
+
 function showCodewareScore() {
     if(codewarsDiv.style.visibility == "hidden") {
         codewarsDiv.style.visibility = "visible";
@@ -8,9 +9,4 @@ function showCodewareScore() {
     else {
         codewarsDiv.style.visibility = "hidden";
     }
-}
-
-function darkMode() {
-    let element = document.body;
-    element.classList.toggle("dark-mode"); 
 }
